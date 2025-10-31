f3621746e1975601d1ba62fa327a13eb08b7b44e
ben-auo <180770701+ben-auo@users.noreply.github.com>
2025-10-31 15:13:22 -0700

    refactor

--- dagu.go
+++ dagu.go
@@ -5,13 +5,8 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
 	"log"
-	"os"
-	"path/filepath"
 	"context"
-	"strings"
-	"syscall"
 	"errors"
 	"net"
 
@@ -20,6 +15,30 @@ import (
 )
 
 var port = "8080"
+
+// https://gist.github.com/schwarzeni/f25031a3123f895ff3785970921e962c
+func GetInterfaceIpv4Addr(interfaceName string) (addr string, err error) {
+    var (
+        ief      *net.Interface
+        addrs    []net.Addr
+        ipv4Addr net.IP
+    )
+    if ief, err = net.InterfaceByName(interfaceName); err != nil { // get interface
+        return
+    }
+    if addrs, err = ief.Addrs(); err != nil { // get addresses
+        return
+    }
+    for _, addr := range addrs { // get ipv4 address
+        if ipv4Addr = addr.(*net.IPNet).IP.To4(); ipv4Addr != nil {
+            break
+        }
+    }
+    if ipv4Addr == nil {
+        return "", errors.New(fmt.Sprintf("interface %s does not have an ipv4 address\n", interfaceName))
+    }
+    return ipv4Addr.String(), nil
+}
 
 func run(logging bool, exe string, args ...string) {
 	var cmd execute.ExecTask
@@ -50,30 +69,6 @@ func run(logging bool, exe string, args ...string) {
 	}
 }
 
-// https://gist.github.com/schwarzeni/f25031a3123f895ff3785970921e962c
-func GetInterfaceIpv4Addr(interfaceName string) (addr string, err error) {
-    var (
-        ief      *net.Interface
-        addrs    []net.Addr
-        ipv4Addr net.IP
-    )
-    if ief, err = net.InterfaceByName(interfaceName); err != nil { // get interface
-        return
-    }
-    if addrs, err = ief.Addrs(); err != nil { // get addresses
-        return
-    }
-    for _, addr := range addrs { // get ipv4 address
-        if ipv4Addr = addr.(*net.IPNet).IP.To4(); ipv4Addr != nil {
-            break
-        }
-    }
-    if ipv4Addr == nil {
-        return "", errors.New(fmt.Sprintf("interface %s does not have an ipv4 address\n", interfaceName))
-    }
-    return ipv4Addr.String(), nil
-}
-
 func main() {
 	// wait for local network
 	gokrazy.WaitFor("net-route")
@@ -85,12 +80,12 @@ func main() {
 	}
 	log.Println("Local IP Address: " + ipAddress)
 
-	// create mount point
-	run(true, "/usr/local/bin/busybox", "mkdir", "-p", "/perm/dagu")
+	// create mount point and use for Dagu storage
+	run(false, "/usr/local/bin/busybox", "mkdir", "-p", "/perm/dagu")
+	run(false, "export", "DAGU_HOME=/perm/dagu")
 
 	// run Dagu
-	home := "DAGU_HOME=/perm/dagu"
-	host := "DAGU_HOST=" + ipAddress
-	port := "DAGU_PORT=" + port
-	run(true, home, host, port, "/usr/local/bin/dagu", "start-all")
+	run(true, "/usr/local/bin/dagu", "server", "--host", ipAddress, "--port", port)
+	run(true, "/usr/local/bin/dagu", "scheduler")
+	run(true, "/usr/local/bin/dagu", "coordinator")
 }
